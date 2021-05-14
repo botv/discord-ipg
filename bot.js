ae146748aef737e876628cf0bcbdb449d8fb280a
Ben Botvinick <benbotvinick@gmail.com>
2021-05-14 04:34:57 -0400

    Code style

--- bot.js
+++ bot.js
@@ -1,11 +1,11 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
 const questions = require('./questions.js');
 
-client.on('message', msg => {
-  if (msg.content === '!ipg') {
+const client = new Discord.Client();
+
+client.on('message', (msg) => {
+  if (msg.content === '!ipg')
     msg.channel.send(questions[Math.floor(Math.random() * questions.length)]);
-  }
 });
 
 client.login(process.env.DISCORD_BOT_TOKEN);
